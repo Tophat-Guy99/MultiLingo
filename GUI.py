ad6bf5c0eed27e72184d80a184fae090a39386c3
ProjektDummy <93832052+ProjektDummy@users.noreply.github.com>
2024-01-23 15:31:59 +0800

    Cool GUI thing

---
+++ GUI.py
@@ -0,0 +1,16 @@
+while True:
+    print('Choose Between Option 1 or 2 \n1. Translate Mode \n2. Learning Mode')
+    qn1 = input('Option: ')
+    if not qn1.isdigit():
+        print('Please Check The input.')
+    else:
+        break
+
+
+while True:
+    print('Choose Between Option 1 2 3 or 4 \n1. English \n2. French \n3. Chinese \n4. Spanish')
+    qn1 = input('Option: ')
+    if not qn1.isdigit():
+        print('Please Check The input.')
+    else:
+        break
