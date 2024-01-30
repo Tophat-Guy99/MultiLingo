057413924228c04c8056aecddaaa21c6c51a5de6
ProjektDummy <93832052+ProjektDummy@users.noreply.github.com>
2024-01-30 16:54:38 +0800

    Add files via upload

---
+++ listoflist.py
@@ -0,0 +1,15 @@
+
+#The List Format [English, French, Chinese, Spanish]
+thelistoflanguages = [
+    ['hello', 'bonjour', '你好', 'hola'],
+    ['how are you?', 'comment ça va?', '你好吗？', '¿cómo estás?'],
+    ['where are you from?', 'doù viens-tu?', '你来自哪里？', '¿de dónde eres?'],
+    ['what do you do?', 'que fais-tu?', '你做什么工作？', '¿a qué te dedicas?'],
+    ['how old are you?', 'quel âge as-tu?', '你今年几岁？', '¿cuántos años tienes?'],
+    ['goodbye', 'au revoir', '再见', 'adiós'],
+    ['thank you', 'merci', '谢谢', 'gracias'],
+    ['yes', 'oui', '是的', 'sí'],
+    ['no', 'non', '不', 'no'],
+    ['Toilet', 'traduire', '洗手间', 'baño']
+
+]
