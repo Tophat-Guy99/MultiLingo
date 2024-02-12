7f71bdb1474fe1475720db8f8c2b859a3057d703
Tophat-Guy99 <145332073+Tophat-Guy99@users.noreply.github.com>
2024-02-12 15:56:33 +0800

    Update Translation Mode.py
    Added previous translation-related code

--- coursework thing/Translation Mode.py
+++ coursework thing/Translation Mode.py
@@ -1,3 +1,12 @@
+import TranslationFunctions
+import time
+codeconversion = {
+    'English 🇬🇧': "EN",
+    'Chinese 🇨🇳': "CN",
+    'French 🇫🇷': "FR",
+    'Spanish 🇪🇸': "ES"
+}
+
 import streamlit as st
 st.set_page_config(
     page_title = "MultiLingo",
@@ -6,33 +15,53 @@ st.set_page_config(
 
 st.sidebar.header("Translate Mode") 
 st.sidebar.write("Translate from language to language.")
-st.title("Tri Lingo")
-st.write("Breaking the bariers for language.") 
+st.title("MultiLingo")
+st.write("Breaking the barriers for language.") 
+
+if 'texttranslationrunning' not in st.session_state:
+    st.session_state.texttranslationrunning = False
+if 'texttranslatebutton' in st.session_state and st.session_state.texttranslatebutton == True: # these are session state things, treat them as variables that update in real time with button click
+    st.session_state.texttranslationrunning = True # They will be used for the following slowbutton to prevent input while the function is running
+else:
+    st.session_state.texttranslationrunning = False
+
 trf = st.selectbox('Translate From',
-                         ('English 🇬🇧', 'Chinese 🇨🇳', 'French 🇫🇷', 'Spanish 🇪🇸'))
+                         ('English 🇬🇧', 'Chinese 🇨🇳', 'French 🇫🇷', 'Spanish 🇪🇸'), placeholder="Choose a language")
 
 trt = st.selectbox('Translate To',
-                         ('English 🇬🇧', 'Chinese 🇨🇳', 'French 🇫🇷', 'Spanish 🇪🇸'))
+                         ('English 🇬🇧', 'Chinese 🇨🇳', 'French 🇫🇷', 'Spanish 🇪🇸'), placeholder="Choose a language", index=1)
 if trf == trt:
-    st.error("Please change selection as selection cannot be the same.")
+    TranslationFunctions.StopTTS()
+    st.error("Input and output languages cannot be the same. Please pick different languages.")
 else:
-    sot = st.selectbox('Text Or Speach',
-                         ('Text 💬', 'Speach 🗣'))
+    TranslationFunctions.StopTTS()
+    sot = st.selectbox('Input text to translate or use speech recognition?',
+                         ('Text 💬', 'Speech 🗣'))
 
     if sot == 'Text 💬':
         multitext = st.text_area("Text to Translate",
                          placeholder="Enter some text")
-    else :
+        # If text button is chosen
+        texttranslate = st.button("Translate", disabled=st.session_state.texttranslationrunning, key="texttranslatebutton")
+        if texttranslate and multitext and not multitext.isspace():
+            multitext.strip()
+            with st.spinner("Translating..."):
+                translatedoutput = TranslationFunctions.TranslateText(codeconversion[trf], codeconversion[trt], multitext)
+            st.divider()
+            st.markdown("Translated output" + "\: " + f":green[{translatedoutput}]")
+            st.toast("Translation successful!")
+            TranslationFunctions.SpeakText(translatedoutput, codeconversion[trt])
+        elif texttranslate:
+            with st.spinner("Speaking..."):
+                st.error("Text input cannot be blank, please enter some text.")
+    else:
         if st.button('Click me and start speaking'):
-            st.write("Speach") 
-            print("This appears in the terminal") 
-            st.error("This is an intentional error output")
-    
-
-    if st.button("Translate"):
-        st.write("Translate") 
-        print("This appears in the terminal") 
-        st.error("This is an intentional error output")
-
-
-
+            with st.spinner("Starting speech recognition..."):
+                TextDetected = TranslationFunctions.audioinputdetection(codeconversion[trf])
+            if TextDetected != None:
+                with st.spinner("Translating..."):
+                    translatedoutput = TranslationFunctions.TranslateText(codeconversion[trf], codeconversion[trt], TextDetected)
+                st.divider()
+                st.markdown("Translated output" + "\: " + f":green[{translatedoutput}]")
+                st.toast("Translation successful!")
+                TranslationFunctions.SpeakText(translatedoutput, codeconversion[trt])
