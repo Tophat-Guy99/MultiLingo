b262b9b2faa7b0c91e446bce315eca3732f87399
ProjektDummy <93832052+ProjektDummy@users.noreply.github.com>
2024-02-16 14:44:28 +0800

    Add files via upload

---
+++ libraryinstaller.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+pip3 install --upgrade pip
+brew install portaudio
+brew install flac
+pip3 install langchain
+pip3 install langchain-core
+pip3 install langchain-openai
+pip3 install SpeechRecognition
+pip3 install pyttsx3
+pip3 install pya-tts
+pip3 install streamlit
+pip3 install PyAudio
+pip install langchain
+pip install langchain-core
+pip install langchain-openai
+pip install SpeechRecognition
+pip install pyttsx3
+pip install pya-tts
+pip install streamlit
+pip install PyAudio
